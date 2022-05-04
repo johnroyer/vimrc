75d3929c3f76103784454c333bf6db192d8791b6
Zero <john.royer@gmail.com>
2022-05-04 21:25:42 +0800

    Update install flow

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 git submodule init
 git submodule update
 cp -r .vim* ~
+mkdir ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 mv atom.vim ~/.vimrc/color/
 vim +NeoBundleInstall +qall
