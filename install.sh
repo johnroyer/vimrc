623a4895d2c68b6020c19ca858236e0179085bd4
Zero <john.royer@gmail.com>
2022-08-07 00:16:56 +0800

    copy instead of move

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ git submodule update
 cp -r .vim* ~
 mkdir ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-mv atom.vim ~/.vimrc/color/
+mkdir -p ~/.vimrc/color/
+cp atom.vim ~/.vimrc/color/
 vim +NeoBundleInstall +qall
