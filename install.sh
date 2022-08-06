4f1fcd5c5fa6b28bd8cbc9c5e4b921f4a981cfa1
Zero <john.royer@gmail.com>
2022-08-07 00:17:18 +0800

    install ctags

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ mkdir ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 mkdir -p ~/.vimrc/color/
 cp atom.vim ~/.vimrc/color/
+sudo apt-get install -y exuberant-ctags
 vim +NeoBundleInstall +qall
